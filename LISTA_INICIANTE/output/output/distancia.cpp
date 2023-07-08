0042c214c89e33bfd5fc179de56cda32f7944e35
Micael Rosário <micaeldevrosario@gmail.com>
2023-07-08 12:53:58 -0300

    +question

---
+++ LISTA_INICIANTE/output/output/distancia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int x, distancia;
+    cin >> x;
+    //Calculo = distancia * minutos
+    distancia = x * 2;
+    cout << distancia << " minutos"<< endl;
+    return 0 ;
+}
